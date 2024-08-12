a2017d4d49794f408815898ab14b9db941f4784f
imtiazsoftic <imtiaz@softic.ai>
2024-08-12 22:36:11 +0600

    controller, repositories, service created

---
+++ repositories/UserRepository.go
@@ -0,0 +1,7 @@
+package repositories
+
+import "github.com/ahmed-afzal1/go-auth/models"
+
+func SignUp(user models.User) (models.User, error) {
+
+}
---
+++ services/UserService.go
@@ -0,0 +1,7 @@
+package services
+
+import "github.com/ahmed-afzal1/go-auth/models"
+
+func SignUp(user models.User) (models.User, error) {
+
+}
