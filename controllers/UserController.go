bf2a8d5930dbd6c6a5e9119494f6d844800c20bd
imtiazsoftic <158676285+imtiazsoftic@users.noreply.github.com>
2024-08-13 10:15:03 +0600

    add gitignore file

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,7 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/ahmed-afzal1/go-auth/models"
+	"github.com/gin-gonic/gin"
+)
 
 func SignUp(c *gin.Context) {
+	var user models.User
 
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
 }
