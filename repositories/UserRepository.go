6c81a3ce2c5fe6c7c0efcda5ad882c5777858865
imtiazsoftic <imtiaz@softic.ai>
2024-08-13 22:36:12 +0600

    signup and generate token

--- repositories/UserRepository.go
+++ repositories/UserRepository.go
@@ -2,10 +2,15 @@ package repositories
 
 import (
 	"github.com/ahmed-afzal1/go-auth/config"
+	"github.com/ahmed-afzal1/go-auth/helper"
 	"github.com/ahmed-afzal1/go-auth/models"
 )
 
 func SignUp(user models.User) (models.User, error) {
+	token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, user.User_id)
+	user.Token = &token
+	user.Refresh_token = &refreshToken
+
 	if err := config.DB.Create(&user).Error; err != nil {
 		return user, err
 	}
